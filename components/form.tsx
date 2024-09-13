b03c7df84576a0ca0022b60cd886b748cfd2f984
Elias Neto <netoeac.dev@gmail.com>
2024-09-12 22:27:36 -0300

    baseline and sections created

--- components/form.tsx
+++ components/form.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -6,102 +6,101 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
-
+} from "@/components/ui/select";
 
 function Form() {
+  return (
+    <div>
+      <Card className="w-[380px] bg-[#E4F0FF] ">
+        <CardHeader>
+          <CardTitle className="font-bold text-2xl">
+            Request a Service
+          </CardTitle>
+        </CardHeader>
 
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-2">
+              <div className="flex flex-col gap-2">
+                <Input
+                  id="name"
+                  placeholder="Your name"
+                  className="bg-white "
+                />
+                <Input
+                  type="number"
+                  id="phone"
+                  placeholder="Phone Number"
+                  className="bg-white"
+                />{" "}
+                {/*Verificar com elias o motivo do mt(Margin-top) não está funcionando */}
+              </div>
 
-    return (
+              <div className="flex flex-col">
+                <Select>
+                  <SelectTrigger id="framework" className="bg-white">
+                    <SelectValue placeholder="Choose a services" />
+                  </SelectTrigger>
 
-        <div>
+                  <SelectContent position="popper">
+                    <SelectItem value="next">Quick Cleaning</SelectItem>
+                    <SelectItem value="sveltekit">Cost-effective</SelectItem>
+                    <SelectItem value="astro">Silent Cleaning</SelectItem>
+                    <SelectItem value="nuxt">Exterior Cleaning</SelectItem>
+                  </SelectContent>
+                </Select>
+              </div>
 
-            <Card className="w-[380px] bg-[#E4F0FF] ">
-                <CardHeader>
-                    <CardTitle className="font-bold text-2xl">Request a Service</CardTitle>
-                    
-                </CardHeader>
+              <div className="flex gap-2">
+                <Input
+                  id="Eircode"
+                  type="number"
+                  placeholder="Eircode"
+                  className="bg-white w-[50%]"
+                />
 
-                <CardContent>
-                    <form>
+                <Input
+                  type="name"
+                  id="phone"
+                  placeholder="Street"
+                  className="bg-white w-[50%]"
+                />
+              </div>
 
-                    <div className="grid w-full items-center gap-4">
+              <div className="flex gap-2">
+                <Input
+                  id="District"
+                  placeholder="District"
+                  className="bg-white w-[50%]"
+                />
 
-                        <div className="flex flex-col space-y-1.5">
-                        
-                           <Input id="name" placeholder="Your name" className="bg-white " />
-                           <Input type="number" id="phone" placeholder="Phone Number" className="bg-white" /> {/*Verificar com elias o motivo do mt(Margin-top) não está funcionando */}
+                <Input
+                  type="number"
+                  id="House number"
+                  placeholder="House number"
+                  className="bg-white w-[50%]"
+                />
+              </div>
+            </div>
+          </form>
+        </CardContent>
 
-                        </div>
-
-                        <div className="flex flex-col space-y-1.5">
-
-                            
-
-                            <Select>
-
-                                <SelectTrigger id="framework" className="bg-white">
-
-                                    <SelectValue placeholder="Choose a services"  />
-
-                                </SelectTrigger>
-
-                                <SelectContent position="popper">
-
-                                <SelectItem value="next">Quick Cleaning</SelectItem>
-                                <SelectItem value="sveltekit">Cost-effective</SelectItem>
-                                <SelectItem value="astro">Silent Cleaning</SelectItem>
-                                <SelectItem value="nuxt">Exterior Cleaning</SelectItem>
-
-                                </SelectContent>
-
-                            </Select>
-
-                        </div>
-
-                        <div className="flex gap-2">
-                        
-                           <Input id="Eircode" 
-                           type="number"
-                           placeholder="Eircode" className="bg-white w-[45%]" />
-                           
-                           <Input type="name" id="phone" placeholder="Street" className="bg-white w-[45%]" /> 
-
-                        </div>
-
-                        <div className="flex gap-2">
-                        
-                           <Input id="District" placeholder="District" className="bg-white w-[45%]" />
-
-                           <Input type="number" id="House number" placeholder="House number" className="bg-white w-[45%]" /> 
-
-                        </div>
-
-                    </div>
-
-                    </form>
-
-                </CardContent>
-
-                <CardFooter>
-                    
-                    <Button className="bg-[#FDE047] text-black hover:bg-yellow-400 w-[65%] h-[50px] rounded-[15px]" >Request Services</Button>
-
-                </CardFooter>
-
-            </Card>
-
-        </div>
-    )
+        <CardFooter>
+          <Button className="bg-[#FDE047] text-black hover:bg-yellow-400 w-full h-[50px] rounded-2xl justify-center items-center">
+            Request Services
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 }
 
-
-export default Form
+export default Form;
