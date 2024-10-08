e9a17e437e0b8c95e64f7712f1403b5eec35f5f3
Peterson Couto <142356426+petersonfernandocouto@users.noreply.github.com>
2024-10-08 16:02:46 -0300

    Atualização useInView em services

--- components/services.tsx
+++ components/services.tsx
@@ -6,9 +6,20 @@ import ResidentialIMG from "../public/assets/services/residential.jpg";
 import WindowIMG from "../public/assets/services/window.jpg";
 import Image from "next/image";
 import ServiceCard from "./serviceCard";
+import { useInView } from "react-intersection-observer";
+
 
 function Services() {
+
+    const { ref: residentialRef, inView: residentialInView } = useInView({triggerOnce: true,});
+    const { ref: commercialRef, inView: commercialInView } = useInView({triggerOnce: true,});
+    const { ref: deepRef, inView: deepInView } = useInView({triggerOnce: true,});
+    const { ref: WindowCleRef, inView: WindowCleInView } = useInView({triggerOnce: true,});
+
   return (
+
+
+
     <div className="flex flex-col justify-around items-center w-full bg-service bg-cover lg:pt-10 pb-20">
       <div className="flex flex-col items-center w-full  mt-10 mb-10 lg:mb-0 ">
         <p className="text-center items-center lg:text-center w-fit px-4 py-2 rounded-full text-emerald-50 font-bold  ">
@@ -21,7 +32,15 @@ function Services() {
       </div>
       <div className="flex flex-col gap-4">
         <div className="flex flex-col gap-10 lg:flex-row px-4 lg:gap-20 lg:mt-12">
-          <div className="flex flex-col items-center text-white lg:w-[500px]">
+
+          <div 
+          ref={residentialRef}
+          className={`flex flex-col items-center text-white lg:w-[500px] transition-all duration-1000  ${
+              residentialInView
+                ? "opacity-100 translate-x-0"
+                : "opacity-0 -translate-x-full"
+            } `}>
+
             <Image
               className="rounded-3xl w-[450px] lg:w-[560px] "
               src={ResidentialIMG}
@@ -39,9 +58,19 @@ function Services() {
                 inviting.
               </p>
             </div>
+
           </div>
 
-          <div className="flex flex-col items-center text-white lg:w-[500px]">
+          <div 
+            ref={commercialRef}
+            className={`flex flex-col items-center text-white lg:w-[500px] transition-all duration-1000  ${
+              commercialInView
+                ? "opacity-100 translate-x-0"
+                : "opacity-0 -translate-x-full"
+            }`}
+              style={{ transitionDelay: commercialInView ? "1s" : "0s" }} // nesta linha eu manipulo o delay. a div irá aparecer após 1 segundo em relação a div que vem antes
+            >
+
             <Image
               className="rounded-3xl w-[450px] lg:w-[560px] "
               src={CommercialIMG}
@@ -59,11 +88,23 @@ function Services() {
                 environments.
               </p>
             </div>
+
           </div>
         </div>
 
         <div className="flex flex-col gap-10 lg:flex-row px-4 lg:gap-20 lg:mt-12">
-          <div className="flex flex-col items-center text-white lg:w-[500px]">
+
+          <div 
+            ref={deepRef}
+            className={`flex flex-col items-center text-white lg:w-[500px] transition-all duration-1000  ${
+              deepInView
+                ? "opacity-100 translate-x-0"
+                : "opacity-0 -translate-x-full"
+            }`}
+              style={{ transitionDelay: deepInView ? "1s" : "0s" }} 
+            >
+
+
             <Image
               className="rounded-3xl w-[450px] lg:w-[560px] "
               src={DeepIMG}
@@ -81,8 +122,21 @@ function Services() {
                 occasions.
               </p>
             </div>
+
+
           </div>
-          <div className="flex flex-col items-center text-white lg:w-[500px]">
+
+
+          <div 
+          ref={WindowCleRef}
+          className={`flex flex-col items-center text-white lg:w-[500px] transition-all duration-1000  ${
+              WindowCleInView
+                ? "opacity-100 translate-x-0"
+                : "opacity-0 -translate-x-full"
+            }`}
+              style={{ transitionDelay: WindowCleInView ? "1.5s" : "0s" }}
+            >
+
             <Image
               className="rounded-3xl w-[450px] lg:w-[560px] "
               src={WindowIMG}
@@ -99,10 +153,14 @@ function Services() {
                 shining, providing a clear view of the world outside.
               </p>
             </div>
+
+
           </div>
         </div>
       </div>
     </div>
+
+
   );
 }
 
